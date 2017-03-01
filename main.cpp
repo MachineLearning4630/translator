f151dfe22803c9709e178b2cf4389a2b5b9aebfc
Chris-Moffitt <moffitt.a.chris@gmail.com>
2017-03-01 11:35:14 -0500

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  //Takes a string input
+  string unTr;
+  cout << "Enter word to be translated\n";
+  cin >> unTr;
+  //Does some magic (to be made later)
+  //outputs translation
+  cout << unTr << "\n";
+  return 0;
+}
